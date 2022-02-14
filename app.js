3d8639475ade699e2825fefa91f51290bfb78992
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-02-13 20:55:12 -0500

    changed spacing and a few wordings of prompts

--- app.js
+++ app.js
@@ -14,22 +14,22 @@ class USSSchwarzenegger extends Ship{
     }
     attack(enemy){
         if(this.accuracy > Math.random()){
-           
-                enemy.hull -= this.firepower
-                alert("You hit the Alien!")
-            console.log("You hit the Alien!")
+           enemy.hull -= this.firepower
+           alert("You hit the Alien! Good Shot!")
+            console.log("You hit the Alien! Good Shot!")
             if(enemy.hull <= 0){
                 enemy.alive = false
                 alert("The Alien has been killed!")
                 console.log("The Alien has been killed!")
             }
-        }else{
-            alert("You missed!")
-            console.log("You missed!!")
+        }
+        else{
+            alert("You missed it!!")
+            console.log("You missed it!!")
         
+        }
     }
-    }
-    }
+}
 
 
 
@@ -44,19 +44,20 @@ class Enemy extends Ship{
         attack(player){
             if(this.accuracy > Math.random()){
                 player.hull-=this.firepower
-                alert(`The enemy hit your ship! You have ${player.hull} hit points left.`)
-                console.log(`The enemy hit your ship! You have ${player.hull} hit points left.`)
+                alert(`OH NO! The enemy has attacked your ship! You have ${player.hull} hit points left.`)
+                console.log(`OH NO! The enemy has attacked your ship! You have ${player.hull} hit points left.`)
                 if(player.hull <= 0){
                     player.alive = false
                     alert("Your ship has been destroyed! Game Over!")
                     console.log("Your ship has been destroyed! Game Over!")
                 }
-            }else{
+            }
+            else{
                 alert("The Alien missed you this time!")
                 console.log("The Alien missed you this time!")
             }
-            }
-    }
+        }
+}
 
 
 
@@ -66,24 +67,22 @@ let badGuy = new Enemy()
 
 let enemyFleet = []
 for (let i = 0; i <Math.floor(Math.random()*5)+6; i++){
-enemyFleet[i] = new Enemy()
-}
+    enemyFleet[i] = new Enemy()}
+
 
 
 
 
 function continueBattle (){
   
-    let cont = prompt(`Should we battle? Your hull strength is at ${hero.hull}`, "Yes or No")
+    let cont = prompt(`Shall we battle? Your hull strength is at ${hero.hull}.`, "Yes or No")
     if(cont.toLowerCase() === "yes"){
         return true
-   
-    }else{
-        
-        (cont.toLowerCase()==='no')
+    }
+    else{
+            (cont.toLowerCase()==='no')
             alert("You have run away like a little baby! Earth will be ruined!")
             return false
-
     }
   
 }
@@ -98,30 +97,24 @@ function battle(player, eF){
         if(player.alive == true){alert('Battle #' + (i + 1))}
         if(continueBattle()=== true){
         
-            
             while(player.alive && eF[i].alive ){
             
             player.attack(eF[i]);
-            if (eF[i].alive ==true)
-            {
+            if (eF[i].alive ==true){
                 eF[i].attack(player)
             }
         }
+        }
+        else{break}
     }
-        else{ break}
-    }
-
-    }
+}
     
-
-    
-    
-    function fight(){
-    let start = prompt("Would you like to battle some aliens?","Yes or No")
+function fight(){
+    let start = prompt("Would you like to battle some aliens today?","Yes or No")
     if(start.toLowerCase()==='yes'){
         battle(hero,enemyFleet)
-        }
-        else{ alert("You coward! Earth will be destroyed!")
+    }
+    else{ alert("You coward! Earth has been destroyed!!!")
     }
 }
 
